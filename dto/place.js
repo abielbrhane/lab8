6e533fa6eb399a28a69e85680cab747e834b6bb3
Abiel Brhane <abielberaki1@gmail.com>
2018-07-05 16:57:13 -0600

    fix

---
+++ dto/place.js
@@ -0,0 +1,9 @@
+class Place{
+    constructor(name, category, location){
+        this.category=category;
+        this.name=name;
+        this.location=location;
+    }
+}
+
+module.exports=Place;
